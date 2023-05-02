8fabb22db028ddf6353f421c30c769d41824803c
Andreas Lindell <85985963+andlind@users.noreply.github.com>
2023-05-02 10:35:19 +0200

    Almond 0.5.0 source build

---
+++ build/source/linux-version/0.5.0/almond-0.5.0/structures.h
@@ -0,0 +1,24 @@
+#ifndef ALMOND_DATA_STRUCTURES_HEADER
+#define ALMOND_DATA_STRUCTURES_HEADER
+
+typedef struct PluginItem {
+        char name[50];
+        char description[100];
+        char command[255];
+        char lastRunTimestamp[20];
+        char nextRunTimestamp[20];
+        char lastChangeTimestamp[20];
+        char statusChanged[1];
+        int active;
+        int interval;
+        int id;
+        time_t nextRun;
+} PluginItem;
+
+typedef struct PluginOutput {
+        int retCode;
+        int prevRetCode;
+        char retString[1500];
+} PluginOutput;
+
+#endif
