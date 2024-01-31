652a79a26c417968316ca7b37f2401f4056a6608
Andreas Lindell <alio@alio-mac-42C0.int.svenskaspel.se>
2024-01-31 11:49:07 +0100

    0.6.2 Adding support for Kafka SSL connection

---
+++ build/source/linux-version/0.6.2/install_almond.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#  ##     ####     ##   ##   ## ##   ###  ##  ### ##  # 
+#   ##     ##       ## ##   ##   ##    ## ##   ##  ## # 
+# ## ##    ##      # ### #  ##   ##   # ## #   ##  ## # 
+# ##  ##   ##      ## # ##  ##   ##   ## ##    ##  ## # 
+# ## ###   ##      ##   ##  ##   ##   ##  ##   ##  ## # 
+# ##  ##   ##  ##  ##   ##  ##   ##   ##  ##   ##  ## # 
+####  ##  ### ###  ##   ##   ## ##   ###  ##  ### ##  #
+#
+#     Almond install script @Andreas_li@hotmail.com
+#
+MAKE=/usr/bin/make
+echo "Setting up installation"
+aclocal
+automake --add-missing
+autoreconf
+./configure --prefix=/opt/almond 
+if [ $? -ne 0 ] 
+then
+	echo "Error running configuration script"
+	exit 2
+fi
+if test -f "$MAKE"; then
+	echo "Running make"
+else
+	echo "Make is missing. Aborting."
+	exit 2
+fi
+/usr/bin/make install
+if [ $? -eq 0 ] 
+then
+	echo "Almond install successful"
+else
+	echo "Error running make"
+	exit 2
+fi
+echo "Copy config files."
+cp conf/plugins.conf /etc/almond/
+cp conf/almond.conf /etc/almond/
+cp conf/aliases.conf /etc/almond/
+cp start_almond.sh /opt/almond/
+cp gardener.py /opt/almond/
+echo "Copy plugin files needed."
+cp -r plugins /opt/almond/
+echo "Setting up log directory"
+mkdir -p /var/log/almond
+echo "Installation complete"
