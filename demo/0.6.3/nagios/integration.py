1a090481046ebcf82a1e116aa857ffe2a52170b6
Andreas Lindell <alio@alio-mac-42C0.int.svenskaspel.se>
2024-02-29 15:22:24 +0100

    New integration file in demo

--- demo/0.6.3/nagios/integration.py
+++ demo/0.6.3/nagios/integration.py
@@ -7,6 +7,7 @@ import time
 import functools
 import subprocess
 import os
+import logging
 
 host_list = []
 inventory_host_list = []
@@ -41,7 +42,9 @@ params = {
 def get_collector_host_list(url):
     global host_list
     global server_list
-    print (url)
+    global logger
+
+    logger.info("Get collector host list from url: " + url)
     response = requests.get(url, params=params, headers=headers)
     data = response.json()
     server_list = data.get('server')
@@ -54,7 +57,9 @@ def get_collector_host_list(url):
 def get_inventory_host_list(path):
     global inventory_host_list
     global inventory
-    print (path)
+    global logger
+
+    logger.info("Getting host list from Nagios config. Path used is: " + path)
     with open(path) as inventory_file:
         inventory = inventory_file.readlines()
         for line in inventory:
@@ -67,20 +72,28 @@ def get_inventory_host_list(path):
 
 def compare_list(list1, list2, set_order):
     global changes
+    global logger
+
     s = set(list1)
     temp = [x for x in list2 if x not in s]
     if len(temp) > 0:
         if set_order == 0:
             print ("These server names are found on Nagios but should not be in use.")
+            logger.info("These server names are found on Nagios but should not be in use.")
         else:
             print ("These server names are found and should be added to Nagios configuration")
+            logger.info("These server names are found and should be added to Nagios configuration")
         print (temp)
+        for x in temp:
+            logger.info("Server : " + x)
         changes += len(temp)
 
 def compare_services():
     global inventory
     global server_list
     global changes
+    global logger
+
     num_service_checks = 0
     inventory_service_list = []
     
@@ -89,11 +102,13 @@ def compare_services():
             inventory_service_list.append(inventory[linenum:linenum+6])
 
     print ("Number of service checks in inventory:", len(inventory_service_list))
+    logger.info("Number of service checks in inventory: " + str(len(inventory_service_list)))
     for x in server_list:
         for y in x['monitoring']:
             num_service_checks += 1
 
     print("Number of service checks in api: " + str(num_service_checks))
+    logger.info("Number of service checks in api: " + str(num_service_checks))
     if (len(inventory_service_list) > num_service_checks):
         changes = changes + len(inventory_service_list) - num_service_checks
     if (num_service_checks > len(inventory_service_list)):
@@ -153,24 +168,44 @@ def createNewConfig(file):
             f.write("\n}\n\n")
     f.close()
 
-def checkNagiosConfig():
+def checkCommandExists(file):
+    print("Check if howru command exists")
+    retVal = False
+    with open(file, 'r') as fp:
+        for line in enumerate(fp):
+            if 'check_howru_service_by_id':
+                retVal = True
+                break
+    return retVal
+                
+def checkNagiosConfig(cmd):
+    global logger
     print ("Checking that Nagios configuration is ok.")
-    nagiosProc = subprocess.run(["/opt/nagios/bin/nagios", "-v", "/opt/nagios/etc/nagios.cfg"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    print ("Use " + cmd)
+    logger.info("Checking that Nagios configutation is ok.")
+    logger.info("Using command: " + cmd)
+    #nagiosProc = subprocess.run(["/opt/nagios/bin/nagios", "-v", "/opt/nagios/etc/nagios.cfg"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    nagiosProc = subprocess.run(cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     return nagiosProc.returncode
 
 def restartNagios():
+    global logger
     print ("Restarting Nagios.")
+    logger.info("Restarting Nagios")
     restartProc = subprocess.run(["/opt/almond/restart_nagios_process.sh"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if restartProc.returncode == 0:
+        logger.info("Restarted Nagios process.")
         print ("Restarted Nagios process.")
     else:
+        logger.warning("Failed to restart Nagios process.")
         print ("Failed to restart Nagios process.")
 
-
-def do_integrate(url,file):
+def do_integrate(url,file, cmd, cfile):
     global host_list
     global inventory_list
+    global logger
 
+    logger.info("Comparing nagios config with api data")
     get_collector_host_list(url)
     get_inventory_host_list(file)
     compare_list(host_list, inventory_host_list, 0)
@@ -178,34 +213,39 @@ def do_integrate(url,file):
     if changes < 1:
         compare_services()
     if changes > 0:
+        if not checkCommandExists(cfile):
+            print("Check against howru api not found in command file. Aborting integration.")
+            logger.critical("No command to check howru api found in Nagios commands file. Aborting integration.")
+            return None
         print ("Creating new config")
+        logger.info("Changes found. We need to update Nagios configuration.")
+        logger.info("Creating a new configuration file for Nagios.")
         createNewConfig(file)
-        if checkNagiosConfig() < 1:
+        if checkNagiosConfig(cmd) < 1:
             restartNagios()
         else:
             print("ERROR\tNagios config error.")
+            logger.warning("Nagios configuration did not pass check. Something is wrong in Nagios configuration!")
     else:
         print ("Nothing to do")
+        logger.info("Nothing to do for now. That is nice, I guess.")
 
 def main():
+    global logger
     parser = argparse.ArgumentParser(description='Almond HowRU API Nagios integration')
     parser.add_argument('-u', '--url', type=str, required=True, help='Url to collector, for an example http://localhost:80')
     parser.add_argument('-f', '--file', type=str, required=True, help='Path to config file, for an example /opt/nagios/etc/conf.d/almond.cfg')
     parser.add_argument('-s', '--sleep', type=int, required=False, default=5, help='Number of minutes between integrations. Default is 5')
+    parser.add_argument('-n', '--nagioscheck', type=str, required=False, default='/opt/nagios/bin/nagios -v /opt/nagios/etc/nagios.cfg', help='Command to check Nagios config')
+    parser.add_argument('-c', '--commandfile', type=str, required=False, default='/opt/nagios/etc/objects/commands.cfg', help='Path to Nagios commands file')
     args = parser.parse_args()
-    #get_collector_host_list(args.url)
-    #get_inventory_host_list(args.file)
-    #compare_list(host_list, inventory_host_list, 0)
-    #compare_list(inventory_host_list, host_list, 1)
-    #if changes < 1:
-    #    compare_services()
-    #if changes > 0:
-    #    print ("Create new config")
-    #    createNewConfig(args.file)
-    #else:
-    #    print ("Nothing to do")
+    logging.basicConfig(filename='/var/log/integration.log', filemode='a', format='%(asctime)s | %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.info("Integration started and arguments parsed.")
     print ("Starting integration with runs every " + str(args.sleep) + " minutes.")
-    schedule.every(args.sleep).minutes.do(functools.partial(do_integrate, args.url, args.file))
+    logger.info("Starting integration with runs every " + str(args.sleep) + " minutes.")
+    schedule.every(args.sleep).minutes.do(functools.partial(do_integrate, args.url, args.file, args.nagioscheck, args.commandfile))
     while True:
         schedule.run_pending()
         time.sleep(1)
