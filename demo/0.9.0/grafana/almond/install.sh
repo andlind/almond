a6cd4afed5b86ac6367c23479a60d6445d0ea19a
Andreas Lindell <alio@alio-mac-42C0.int.svenskaspel.se>
2024-07-05 11:17:51 +0200

    Adding almond installation to Grafana container in demo

---
+++ demo/0.9.0/grafana/almond/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Installing Almond on container almond_grafana"
+echo "Installing dependencies"
+docker cp ../../alpine/almond/almond-0.9.0.2.alpine.aarch64.tar.gz almond_grafana:/tmp
+docker exec -u 0 almond_grafana apk update
+docker exec -u 0 almond_grafana apk add --no-cache perl sysstat bash python3 py3-psutil procps busybox iputils json-c librdkafka
+docker exec -u 0 almond_grafana tar xfvz /tmp/almond-0.9.0.2.alpine.aarch64.tar.gz
+echo "Installing precompiled Almond"
+docker exec -u 0 almond_grafana /bin/bash -c "pwd && cd almond-0.9.0.2.alpine.aarch64 && ./install.sh"
+echo "Installation finished"
+echo "Copy configuration files"
+docker cp almond.conf almond_grafana:/etc/almond/almond.conf
+docker cp plugins.conf almond_grafana:/etc/almond/plugins.conf
+echo "Start Almond"
+docker exec -u 0 almond_grafana /bin/bash -c "/opt/almond/start_almond.sh &"
+echo "Done"
