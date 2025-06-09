43f0ca307db3475131542b55e774109840998e76
Andreas Lindell <alio@alio-mac-42C0.int.svenskaspel.se>
2025-06-09 12:33:55 +0200

    Update docker build to buildx

--- demo/0.9/01_stage1.sh
+++ demo/0.9/01_stage1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting redis database"
-docker build -t redisdemo redis
-nohup docker run --rm --platform linux/amd64 --net almond-redpanda-quickstart_almond-demo --name redis_demo -d -v ~/almond_demo/data:/opt/almond/data -p 6379:6379 redisdemo
+docker buildx build --platform linux/amd64 -t redisdemo redis
+nohup docker run --privileged --rm --platform linux/amd64 --net almond-redpanda-quickstart_almond-demo --name redis_demo -d -v ~/almond_demo/data:/opt/almond/data -p 6379:6379 redisdemo
 echo "Starting custom (backend) app"
-docker build -t custapplication custapp
+docker buildx build --platform linux/amd64 -t custapplication custapp
 nohup docker run --rm --platform linux/amd64 --net almond-redpanda-quickstart_almond-demo --name customapp_demo -d -v ~/almond_demo/data:/opt/almond/data -p 8076:8099 custapplication
 echo "Starting webbapplication"
-docker build -t webapp webapp
+docker buildx build --platform linux/amd64 -t webapp webapp
 nohup docker run --rm --platform linux/amd64 --net almond-redpanda-quickstart_almond-demo --name web_demo -d -v ~/almond_demo/data:/opt/almond/data -p 8075:80 webapp
 echo "Stage 1 done"
