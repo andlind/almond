65ba98a1234eab3c6bdbb265ff296183d5c113c8
Andreas Lindell <alio@alio-mac-42C0.int.svenskaspel.se>
2025-06-09 12:37:39 +0200

    Update docker builds in demo

--- demo/0.9/02_stage2.sh
+++ demo/0.9/02_stage2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting howru-api"
-docker build -t howruapi api 
+docker buildx build --platform linux/amd64 -t howruapi api 
 nohup docker run --rm --platform linux/amd64 --name howru_demo --net almond-redpanda-quickstart_almond-demo -d -v ~/almond_demo/data:/opt/almond/data -p 8085:80 howruapi
 echo "Stage 2 done"
