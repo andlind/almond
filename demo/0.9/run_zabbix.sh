810b4361c2876c57b047ea504e261f7711f573e1
Andreas Lindell <andreas.lindell@almondmonitor.com>
2025-11-03 23:42:47 +0100

    Run integration in bg in Zabbix demo

--- demo/0.9/run_zabbix.sh
+++ demo/0.9/run_zabbix.sh
@@ -40,4 +40,5 @@ docker run -d --name zabbix-agent \
   -e HOSTNAME=agent-host \
   zabbix/zabbix-agent:alpine-latest
 echo "Start the integration"
-docker exec -u root zabbix-server python3 /opt/almond/integration.py > /dev/null 2>&1 &
+docker exec -u root zabbix-server python3 /opt/almond/integration/integration.py > /dev/null 2>&1 &
+
