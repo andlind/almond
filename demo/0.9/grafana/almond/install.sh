b3346f45017b978802da4b1f3ff22369a126490e
Andreas Lindell <andreas.lindell@almondmonitor.com>
2026-01-28 12:19:55 +0100

    Update to version 0.9.23 in demo

--- demo/0.9/grafana/almond/install.sh
+++ demo/0.9/grafana/almond/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Installing Almond on container almond_grafana"
 echo "Installing dependencies"
-docker cp ../../alpine/almond/almond-0.9.18.alpine.aarch64.tar.gz almond_grafana:/tmp
+docker cp ../../alpine/almond/almond-0.9.23.alpine.aarch64.tar.gz almond_grafana:/tmp
 docker exec -u 0 almond_grafana apk update
 docker exec -u 0 almond_grafana apk add --no-cache perl sysstat bash python3 py3-psutil procps busybox iputils json-c librdkafka
 docker exec -u 0 almond_grafana apk add --no-cache openssl musl libc6-compat
-docker exec -u 0 almond_grafana tar xfvz /tmp/almond-0.9.18.alpine.aarch64.tar.gz
+docker exec -u 0 almond_grafana tar xfvz /tmp/almond-0.9.23.alpine.aarch64.tar.gz
 echo "Installing precompiled Almond"
-docker exec -u 0 almond_grafana /bin/bash -c "pwd && cd almond-0.9.18.alpine.aarch64 && ./install.sh"
+docker exec -u 0 almond_grafana /bin/bash -c "pwd && cd almond-0.9.23.alpine.aarch64 && ./install.sh"
 echo "Installation finished"
 echo "Copy configuration files"
 docker cp almond.conf almond_grafana:/etc/almond/almond.conf
