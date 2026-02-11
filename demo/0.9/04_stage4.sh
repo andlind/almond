39a8ebe783f302a7df56e0e03e5854270700f254
Andreas Lindell <andreas.lindell@almondmonitor.com>
2026-02-11 09:25:18 +0100

    Space in run

--- demo/0.9/04_stage4.sh
+++ demo/0.9/04_stage4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start prometheus"
 docker build -t my-prometheus prometheus
-docker run --rm --name almond_prometheus --network almond-demo --network-alias prometheus-d -v ~/almond_demo/data:/opt/almond/data -p 9090:9090 my-prometheus
+docker run --rm --name almond_prometheus --network almond-demo --network-alias prometheus -d -v ~/almond_demo/data:/opt/almond/data -p 9090:9090 my-prometheus
