4d838fad10b630bf13a7fdbea3c9b82e959ff5ae
Andreas Lindell <andreas.lindell@almondmonitor.com>
2026-02-17 09:48:13 +0100

    Cleanup

--- demo/0.6.3/stage4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Start prometheus"
-docker build -t my-prometheus prometheus
-docker network create almond-demo
-docker run --rm --name almond_prometheus --network almond-demo --network-alias prometheus -d -p 9090:9090 my-prometheus
--- demo/0.6.3/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/bin/bash stage1.sh
-/bin/bash stage2.sh
-/bin/bash stage3.sh
-/bin/bash stage4.sh
-/bin/bash stage5.sh
