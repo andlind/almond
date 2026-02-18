4ffea1b6dd5582b2117406d80f33ff5a190c2ab9
Andreas Lindell <andreas.lindell@almondmonitor.com>
2026-02-18 08:55:16 +0100

    Remove shared folder in demo

--- demo/0.9/03_stage3.sh
+++ demo/0.9/03_stage3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Starting Nagios"
 docker buildx build --platform linux/amd64 -t nagios nagios
-nohup docker run --rm --platform linux/amd64 --name nagios4 --net almond-redpanda-quickstart_almond-demo -d -v ~/almond_demo/nagios/etc:/opt/nagios/etc -v ~/almond_demo/nagios/plugins:/opt/Custom-Nagios-Plugins --entrypoint "/usr/bin/supervisord" -p 8180:80 nagios
+nohup docker run --rm --platform linux/amd64 --name nagios4 --net almond-redpanda-quickstart_almond-demo -d -v ~/almond_demo/nagios/plugins:/opt/Custom-Nagios-Plugins --entrypoint "/usr/bin/supervisord" -p 8180:80 nagios
+#nohup docker run --rm --platform linux/amd64 --name nagios4 --net almond-redpanda-quickstart_almond-demo -d -v ~/almond_demo/nagios/etc:/opt/nagios/etc -v ~/almond_demo/nagios/plugins:/opt/Custom-Nagios-Plugins --entrypoint "/usr/bin/supervisord" -p 8180:80 nagios
+#nohup docker run --rm --platform linux/amd64 --name nagios4 --net almond-redpanda-quickstart_almond-demo -d --entrypoint "/usr/bin/supervisord" -p 8180:80 nagios
