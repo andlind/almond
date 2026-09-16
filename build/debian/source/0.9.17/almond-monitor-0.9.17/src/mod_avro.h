395d9a3b106dd41a099e5d21e5e17b0a295de7e5
Andreas Lindell <andreas.lindell@almondmonitor.com>
2026-09-16 15:15:03 +0200

    [26.1.2] Debian build files

--- build/debian/source/0.9.17/almond-monitor-0.9.17/src/mod_avro.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODKAFKA_H
-#define MODKAFKA_H
-
-int send_message_to_kafka(char*, char*, char*);
-int send_ssl_message_to_kafka(char*, char*, char*, char*, char*, char*);
-int send_avro_message_to_kafka(char*, char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, int);
-int send_ssl_avro_message_to_kafka(char*, char*, char*, char*, char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, const char*, int);
-
-#endif // MODKAFKA_H 
